6ceb9da45a8942f75efe40c492819c225e4d54f7
Tom Elliff <tomelliff@gmail.com>
2017-02-17 19:41:04 +0000

    First stab at requeueing from a dead letter queue.
    Heavily influenced by the good work done by Jimdo at https://github.com/Jimdo/sqs-dead-letter-handling
    
    Not overly happy with the boto3 sqs interface where you can't just read a message and then directly pass the message object to send_message(s) or delete_message(s) without manipulation.
    The fact that send_message(s) and delete_message(s) have different interfaces as well (to closely match the REST API) is annoying too.
    The golang SDK used by Jimdo's version leads to a much cleaner interface by default.
    Might refactor this to tidy it up a bit but it works for now.

---
+++ requeue.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import boto3
+import os
+
+active_queue_name = os.environ['QUEUE_NAME']
+dead_letter_queue_name = active_queue_name + "_dead_letter"
+
+sqs = boto3.resource('sqs')
+
+active_queue = sqs.get_queue_by_name(QueueName=active_queue_name)
+dead_letter_queue = sqs.get_queue_by_name(QueueName=dead_letter_queue_name)
+
+while True:
+    messages = dead_letter_queue.receive_messages(
+                                        MaxNumberOfMessages=10,
+                                        WaitTimeSeconds=20,
+                                        VisibilityTimeout=20)
+    number_of_messages = len(messages)
+    if number_of_messages == 0:
+        print('Requeuing messages done.')
+        break
+    else:
+        print('Moving {} message(s)...'.format(number_of_messages))
+        requeued_messages_to_send = []
+        requeued_messages_to_delete = []
+        for message in messages:
+            requeued_messages_to_send.append({
+                'Id': message.message_id,
+                'MessageBody': message.body
+            })
+            requeued_messages_to_delete.append({
+                'Id': message.message_id,
+                'ReceiptHandle': message.receipt_handle
+            })
+        active_queue.send_messages(Entries=requeued_messages_to_send)
+        dead_letter_queue.delete_messages(Entries=requeued_messages_to_delete)
---
+++ seed_queue.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# Taken from https://github.com/boto/boto3/issues/324#issuecomment-151563366
+import boto3
+import sys
+
+"""Bulk loads test messages on to a given queue.
+
+Pass queue name when calling script.
+eg. python seed_queue.py test_queue
+"""
+
+sqs_queue_name = sys.argv[1]
+print(sqs_queue_name)
+
+sqs = boto3.resource('sqs')
+queue = sqs.get_queue_by_name(QueueName=sqs_queue_name)
+
+for i in range(10):
+    messages = []
+    for j in range(10):
+        n = str(i * 10 + j)
+        messages.append({
+            "Id": n,
+            "MessageBody": "Message %s" % n
+        })
+    queue.send_messages(Entries=messages)
