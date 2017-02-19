625f05730587132853e90e5a138726862429cb0e
Tom Elliff <tomelliff@gmail.com>
2017-02-19 15:49:13 +0000

    Switch Terraform example to source direct from GitHub instead of a local source

--- terraform/usage/example.tf
+++ terraform/usage/example.tf
@@ -1,9 +1,10 @@
 ###################################################################################################
 
 module "requeue" {
-  source = "../modules/requeue_lambda"
-  queue_name = "queue_name"
+  source                       = "github.com/tomelliff/sqs-dead-letter-requeue//terraform/modules/requeue_lambda"
+  queue_name                   = "queue_name"
   lambda_function_package_path = "../../requeue_lambda.zip"
 }
 
 ###################################################################################################
+
